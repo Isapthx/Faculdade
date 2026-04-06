5a03d1384b4c889a136b8d4c893ca2b927d55a6c
Natanrezendee <natan.kiffer.magalhaes@outlook.com>
2026-04-06 19:53:44 -0300

    Front test

---
+++ 4POA/ap10-front/static/js/index.js
@@ -0,0 +1,23 @@
+function abrirMatriz(){
+
+window.location.href = "/matriz";
+
+}
+
+function abrirEmentas(){
+
+window.location.href = "/ementas";
+
+}
+
+function abrirMatrizDescrita(){
+
+window.location.href = "/matriz-descrita";
+
+}
+
+function abrirPdf(){
+
+window.location.href = "/pdf";
+
+}

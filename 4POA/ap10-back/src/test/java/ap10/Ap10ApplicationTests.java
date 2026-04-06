d9bd8a434a0ada7370f1ae297fd48feaaff711cd
isapthx <piresisaacc@gmail.com>
2026-04-06 19:20:34 -0300

    adicionando projeto maven e pasta para o front

---
+++ 4POA/ap10-back/src/test/java/ap10/Ap10ApplicationTests.java
@@ -0,0 +1,13 @@
+package ap10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ap10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

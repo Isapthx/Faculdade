e9817541d806ab374d4b932934aeb59a78d3da67
isapthx <piresisaacc@gmail.com>
2026-04-06 21:31:38 -0300

    alterando a estrutura dos arquivos front

---
+++ 4POA/ap10-front/static/js/api.js
@@ -0,0 +1,20 @@
+const API_BASE_URL = "http://localhost:8080/api";
+
+export const API = {
+    async getDisciplinas() {
+        const res = await fetch(`${API_BASE_URL}/disciplinas`);
+        return await res.json();
+    },
+    async getDisciplinaPorId(id) {
+        const res = await fetch(`${API_BASE_URL}/disciplinas/${id}`);
+        return await res.json();
+    },
+    async enviarAvaliacao(dados) {
+        const res = await fetch(`${API_BASE_URL}/avaliacoes`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(dados)
+        });
+        return await res.json();
+    }
+};
